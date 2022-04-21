4c127a5841357561adafdf315b01333fff33787b
Dobcsi26 <dobmarag@gmail.com>
2022-04-22 00:19:08 +0200

    Firebase added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'jobling-web',
+    appId: '1:431629110459:web:08fbbb2b22bb09ed572d9e',
+    storageBucket: 'jobling-web.appspot.com',
+    apiKey: 'AIzaSyA9bAX3biRbTMc6WQQL5z0WmXcPFW_lIOY',
+    authDomain: 'jobling-web.firebaseapp.com',
+    messagingSenderId: '431629110459',
+    measurementId: 'G-FHP7TZZQGM',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'jobling-web',
+    appId: '1:431629110459:web:08fbbb2b22bb09ed572d9e',
+    storageBucket: 'jobling-web.appspot.com',
+    apiKey: 'AIzaSyA9bAX3biRbTMc6WQQL5z0WmXcPFW_lIOY',
+    authDomain: 'jobling-web.firebaseapp.com',
+    messagingSenderId: '431629110459',
+    measurementId: 'G-FHP7TZZQGM',
+  },
   production: false
 };
 
